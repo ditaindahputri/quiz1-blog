e2894684a6acfc546266b7da1b7fe0d994ad9f8c
ditaindahputri <ditaindahpratamaputri@gmail.com>
2020-07-03 22:12:32 +0700

    Upload Kedua

---
+++ database/migrations/2020_06_29_114946_table_user.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TableUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_user',function(Blueprint $table) {
+            $table->increaments('user_id');
+            $table->string('user_name',100);
+            $table->string('user_password',100);
+            $table->string('user_name'100);
+            $table->char('user_role',50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_user');
+    }
+}
